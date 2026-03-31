74840bc7a0b6556a05bf99956b06f72994633c1e
galeano <jhongaleanomartin@gmail.com>
2026-03-30 22:06:59 -0500

    se agrego el primer diseño del miniplayer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './widget/outfit_widget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Combina tu ropa',
+      debugShowCheckedModeBanner: false,
+
+      home: OutfitWidget(),
     );
   }
 }
