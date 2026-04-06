609dbe314c79d613cee53d697186dae2d16726f5
sofia <sofis.devops@gmail.com>
2026-04-06 14:31:04 -0500

    Feat: Ajustes en el diseño de la vista home y favoritos. Conexion de nuevo con la api de FAKE STORE API

--- lib/models/models_api.dart
+++ lib/models/models_api.dart
@@ -24,28 +24,18 @@ class Producto {
     'image': image,
   };
 
-  factory Producto.fromMap(Map<String, dynamic> map) {
-  // 1. Obtenemos la lista de imágenes
-  final List<dynamic> imagesList = map['images'] ?? [];
-  String firstImage = 'https://via.placeholder.com/150';
-
-  if (imagesList.isNotEmpty) {
-    // 2. Limpiamos la URL de corchetes, comillas y espacios extras
-    firstImage = imagesList[0].toString()
-        .replaceAll('[', '')
-        .replaceAll(']', '')
-        .replaceAll('"', '');
-  }
-
-  return Producto(
-    id: map['id'].toString(),
-    title: map['title'] ?? 'Sin título',
-    price: (map['price'] as num?)?.toDouble() ?? 0.0,
-    description: map['description'] ?? 'Sin descripción',
-    category: map['category'] != null ? map['category']['name'].toString() : 'Sin categoría',
-    image: firstImage, // Ya está limpia para Image.network
+  factory Producto.fromMap(Map<String, dynamic> map) => Producto(
+    id: '${map['id'] ?? ''}',
+    title: (map['title']) ?? ' ',
+    price: (map['price']) ?? 0.0,
+    description: (map['description']) ?? ' ',
+    category: (map['category']) ?? 'Unknown',
+    image: (map['image']) ?? 'https://via.placeholder.com/150',
   );
+
 }
+  
+
 
   // factory Producto.fromMap(Map<String, dynamic> map) => Producto(
   //   id: '${map['id'] ?? ''}',
@@ -55,4 +45,4 @@ class Producto {
   //   category: (map['category']) ?? 'Unknown',
   //   image: (map['image']) ?? 'https://via.placeholder.com/150',
   // );
-}
+
